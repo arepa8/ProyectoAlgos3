1b5eb8b00af73e335080dde14ea92062a96b380c
Andres Hernandez <andresrhernandezm@gmail.com>
2014-11-21 01:41:30 -0430

    Lectura de archivo
    Arregle la lectura con scan pero falta guardar lo que se lee

--- Rutas.java
+++ Rutas.java
@@ -6,16 +6,33 @@
  *
  */
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.*;
+
+
 public class Rutas {
 	
 	private ArrayList<Ciudad> ciudades;
 	private ArrayList<Autopista> autopistas;
 
-	public void obtenerGrafo(){
-		//Lectura del grafo
+	public static void obtenerGrafo(String archivo)
+	
+		throws IOException {
+
+	    File datos = new File (archivo);
+	    Scanner scan = new Scanner(datos);
+
+	    while (scan.hasNextLine()){
+
+	    	String line = scan.nextLine();
+	    	System.out.println(line);
+	    }
+
+	    scan.close();
 	}
 
-	public modificarGrafo(){
+	/*public modificarGrafo(){
 		//se modifica el grafo para usar solo el costo de las aristas
 	}
 
@@ -25,5 +42,12 @@ public class Rutas {
 
 	public static void main(String[] args) {
 		
+	}*/
+
+	public static void main(String[] args) {
+		try{
+			obtenerGrafo(args[0]);
+		}
+		catch(Exception e){System.out.println("Error al leer el archivo");}
 	}
 }
